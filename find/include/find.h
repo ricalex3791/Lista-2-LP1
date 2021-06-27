d46476f9e8203b2d4f8d35d0dc7e1a613aff055c
nerfet <ric.alex55@yahoo.com>
2021-06-27 19:50:03 -0300

    Update find.h

--- find/include/find.h
+++ find/include/find.h
@@ -10,14 +10,19 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*! s
+ * @param first Ponteiro para o primeiro elemento do range
+ * @param last Ponteiro para a posição logo após o último elemento do range
+ * @param value valor a ser comparado na função
+ * @param eq A função de comparação que retorna true quando o primeiro parâmetro é igual ao segundo
+ * 
+ * @return Um iterator para o primeiro elemento no range igual ao valor recebido por parâmetro. Se o valor não for encontrado no range, a função retorna o iterator last.
  */
 template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
 {
     // TODO
-     while (first!=last) {
+    while (first!=last) {
          if(*first==value){
             return first;
          }
