1514884228e33e568a883f0f13d4f9dcb39355cc
nerfet <ric.alex55@yahoo.com>
2021-06-27 12:13:01 -0300

    Update find.h

--- find/include/find.h
+++ find/include/find.h
@@ -17,7 +17,13 @@ template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
 {
     // TODO
-    return first;
+     while (first!=last) {
+         if(*first==value){
+            return first;
+         }
+        first++;
+    }
+    return last;
 }
 
 }
