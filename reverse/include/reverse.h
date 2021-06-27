ec66f1f5a8d953dbd9a37b2d0ca687faed602036
nerfet <ric.alex55@yahoo.com>
2021-06-27 19:50:45 -0300

    Update reverse.h

--- reverse/include/reverse.h
+++ reverse/include/reverse.h
@@ -17,37 +17,12 @@ template<class BidirIt>
 void reverse(BidirIt first, BidirIt last)
 {
     // TODO
-    // for (int i = 0; i < (arr.size()/2); i++){
-    //     std::swap(arr[i],arr[arr.size()-i-1]);
-    // }
-    //BidirIt a;
-    // while (first!=(last/2)) {
-    //     a = *last;
-    //     *last = *first;
-    // }
-    // BidirIt temp=0;
-    // while(first!=last){
-    //     temp++;
-    //     first++;
-
-    // }
     BidirIt temp = last;
     for(BidirIt i=first; i!=last; i++){
         std::swap(*first, *last);
         first++;
         last--;
     }
-    // for(BidirIt i=first; i!=(temp); i++){
-    //     std::swap(*first, *last);
-    // }
-
-    // for(BidirIt i = first; i < last; i++){
-    //     for(BidirIt j = i; j < last; j++){
-    //         if(cmp(*i,*j) == false){
-    //             std::swap(*i, *j);
-    //         }
-    //     }
-    // }
 
 }
 
